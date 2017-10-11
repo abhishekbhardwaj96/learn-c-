ad7d688d0d942434bfffde34c49f7f292e879316
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2017-10-11 21:56:46 +0530

    gloabal variable use

---
+++ global variable.cpp
@@ -0,0 +1,16 @@
+/*extern :- Just for understanding extern is used to declare a global variable or function in another file. The extern modifier is 
+most commonly used when there are two or more files sharing the same global variables or functions 
+
+byte :-byte is a distinct type that implements the concept of byte as specified in the C++ language definition.
+Like char and unsigned char, it can be used to access raw memory occupied by other objects (object representation), but unlike those
+ types, it is not a character type and is not an arithmetic type. A byte is only a collection of bits, and only bitwise logic operators are defined for it.*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int g=40;
+int main(){
+	int g;
+	g=20;
+	cout << g;
+}
