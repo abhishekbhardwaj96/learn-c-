76d5752a5ca3ce15678a3717ddf184693ac12bd3
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2017-10-12 22:43:29 +0530

    distinguish b/w signed and unsigned

---
+++ singed or unsigned number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	short int i;
+	unsigned short int j;
+	j=50000;
+	
+	i=j;
+	cout << "value of i " << i;
+	cout << " \n value of j " <<j;
+	return 0 ;
+}
