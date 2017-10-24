89cd0246d6254a3691a38da5d3c8327fba919a9a
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2017-10-24 23:48:51 +0530

    program to print a specific pattern

---
+++ pattern2.cpp
@@ -0,0 +1,20 @@
+/*
+pattern will be 
+*
+**
+***
+****
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for (i=0; i < 5; i++){
+		for (j=0; j < i; j++){
+			cout << "*";
+		}
+		cout <<endl;
+	}
+	return 0;
+}
