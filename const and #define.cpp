ccd6f01b35f84d2c681b14098ea1aee8a01cdd19
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-02 19:32:57 +0530

    Avoid conio.h and getch() in your program

--- const and #define.cpp
+++ const and #define.cpp
@@ -1,7 +1,7 @@
 /*Constants refer to fixed values that the program may not alter and they are called literals.*/
 
-#include<iostream>
-#include<conio.h>
+#include<iostream> // try to avoid conio.h header file. It would be good for your PC's memory.
+// conio.h is used for some purposes .
 using namespace std;
 #define LENGTH 10
 #define BREADTH 20
